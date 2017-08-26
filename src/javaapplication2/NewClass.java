d253b8666bc82947815369d94bf8a0d35f83dbab
Dan Khanh <khanhndd00331@fpt.edu.vn>
2017-08-26 10:15:21 +0700

    Upload project.

---
+++ src/javaapplication2/NewClass.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author DanOliver
+ */
+public class StudentO1 {
+
+    public static void main(String[] args) {
+        String name = "Nam;Toan;Dung;Truong;Linh"
+        String[] arrayName = names.split(",");
+        for (int i = 0; i < arrayName.lenght; i++) {
+            system.out.printIn(arrayName[i]);
+        }
+        for (String name : arrayName) {
+            System.out.println(name);
+            
+        }
+
+    }
+}
