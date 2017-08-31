5b701037a7f9b560e24521f08f69b8a88f94f339
Dan Khanh <khanhndd00331@fpt.edu.vn>
2017-08-31 10:23:07 +0700

    uploadfile

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -5,17 +5,47 @@
  */
 package javaapplication2;
 
-/**
- *
- * @author DanOliver
- */
+import java.util.Scanner;
+
 public class JavaApplication2 {
 
-    /**
-     * @param args the command line arguments
-     */
+  public static void menuConsole(){
+      System.out.println("=============Menu Console=============");
+      System.out.println("1. Hiện tên sinh viên");
+      System.out.println("2. Thêm sinh viên mới");
+      System.out.println("3. Đặt sinh viên");
+      System.out.println("4. Xóa sinh viên");
+      System.out.println("5. Thoát");
+      System.out.println("--------------------------------------");
+      System.out.println("Hãy điền số mà bạn muốn");
+      Scanner scanner=new Scanner(System.in);
+      int select=scanner.nextInt();
+      switch(select){
+          case 1:
+              System.out.println("Bạn chọn: \"Hiển thị danh sách sinh viên\"");
+              break;
+          case 2:
+              System.out.println("Bạn chọn: \"Thêm sinh viên mới\"");
+              break;
+          case 3:
+              System.out.println("Bạn chọn: \"Đặt sinh viên\"");
+              break;
+          case 4:
+              System.out.println("Bạn chọn: \"Xóa sinh viên\"");
+              break;
+          case 5:
+              System.out.println("");
+              break;
+          case 6:
+              System.out.println("Number Invalid");
+              
+      }
+  }
+    
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        menuConsole();
+        
     }
     
 }
